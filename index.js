1b5af2d6dfd3ba58b17b704373c58f39a5aea476
RobertCummings Jr <robkcjr010210@gmail.com>
2024-11-21 20:30:32 -0800

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// alert displays to the user
+alert ('Hello');
+
+// console.log displays in the deve tools
+console.log('How are you all doing');
+
+// Combo Lock Numbers
+const comboLockNumberOne = 40 - 30;
+console.log(comboLockNumberOne);
+const comboLockNumberTwo = 40 - 0;
+console.log(comboLockNumberTwo);
+const comboLockNumberThree = 40 - 1;
+console.log(comboLockNumberThree);
